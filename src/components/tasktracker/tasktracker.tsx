bad1d088a5a5703db72d8789a22bdeee9deb3696
Austin-ALIT <austin@al.co.za>
2023-07-13 13:37:19 +0200

    Added Styling & useRef for the input

--- src/components/tasktracker/tasktracker.tsx
+++ src/components/tasktracker/tasktracker.tsx
@@ -1,7 +1,10 @@
 // Create a functional component named TaskTracker in the 'components' directory.
 
+// (optional) Add styling or use a UI library like Material-UI to improve the appearance of the task tracker.
+import styles from '../../styles/components/tasktracker.module.css';
+
 // Inside the component, import the useState hook from the 'react' package.
-import { ChangeEvent, useEffect, useState } from "react"
+import { ChangeEvent, useState, useRef } from "react";
 
 type Task = {
     name: string, 
@@ -11,17 +14,22 @@ type Task = {
 export default function TaskTracker(){
     // Use the useState hook to define a tasks state variable, initialized as an empty array.
     const [tasks, setTasks] = useState<Task[]>([{name: 'Example Task', completed: false}]);
-    
-    let newTask:Task = {name: '', completed: false};
-
-    function taskSaveHandler(e: ChangeEvent<HTMLInputElement>){
-        newTask.name = e.target.value;
-    }
+    const newTaskRef = useRef<HTMLInputElement>(null);
 
     // Implement an event handler for the add button that adds a new task to the tasks array.
     function addTaskHandler(event: any){
         event.preventDefault() // To prevent page refreshing onClick
-        setTasks((prevTask) => [...prevTask, newTask]);
+        let tName = newTaskRef.current!.value;
+        
+        // Input Checking
+        if (tName === ''){
+            alert('Task cannot be empty');
+            return;
+        }
+
+        // Adding task and clearing input
+        setTasks((prevTask) => [...prevTask, {name: tName, completed: false}]);
+        newTaskRef.current!.value = '';
     }
     
     // Implement an event handler for the checkbox that updates the completed status of the corresponding task.
@@ -38,34 +46,31 @@ export default function TaskTracker(){
         setTasks( (prevTasks) => {
                 let newTasks = [...prevTasks];
                 newTasks.splice(ind, 1);
-                console.log(ind);
                 return(newTasks);
             }
         );
     }
     
-    function checkHandler(task: Task): boolean{
-        return task.completed;
-    }
-    
     return(
-        <div >
+        <div className={styles.topdiv}>
             {/* // Display an input field and a button to add tasks. */}
-            <div>
+            <div className={styles.inputdiv}>
                 <label> Input Task: </label>
-                <input onChange={taskSaveHandler}></input>
-                <button onClick={addTaskHandler}> Add Tasks </button>
+                {/* <input onChange={taskSaveHandler}/> */}
+                <input ref={newTaskRef}/>
+                <button onClick={addTaskHandler} className={styles.inputbutton}> Add Task </button>
             </div>
             
             {/* // Display the list of tasks below the input field, showing the task name and a checkbox to mark the task as completed. */}
-            <ul>             
+            <ul className={styles.tasklist}>             
                 {tasks.map((t, ind) => {
                     return(
-                            <li key={ind}>
+                            <li key={ind} className={styles.taskitem}>
                                 <input type="checkbox" onChange={(e: any) => {taskCompleteHandler(t,e)}} checked={t.completed} id={t.name}></input> 
-                                <label> {t.name} </label> 
-                                <label> ({t.completed? 'done' : 'todo'}) </label> 
-
+                                
+                                {/* // Strikethrough if task is completed */}
+                                <label style={{textDecoration: (t.completed? 'line-through' : 'none')}}> {t.name}  </label> 
+                                
                                 {/* // Add a delete button next to each task that removes the task from the tasks array when clicked. */}
                                  <button onClick={(e: any) => taskDeleteHander(e, ind)}> Remove </button> 
                             </li>
@@ -76,4 +81,3 @@ export default function TaskTracker(){
     )
 }
 
-// Stuff that will bankrupt me: Strikethrough if task is completed
