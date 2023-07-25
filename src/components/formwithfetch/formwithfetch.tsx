50a5c402aa14a35260dc9dbe3b3d071c872d2530
Austin-ALIT <austin@al.co.za>
2023-07-25 11:51:21 +0200

    Implimented functionality & bigfixes

--- src/components/formwithfetch/formwithfetch.tsx
+++ src/components/formwithfetch/formwithfetch.tsx
@@ -1,23 +1,32 @@
+import useFormInput from "@/hooks/useforminput/useforminput"
+import useDataFetching from "@/hooks/usedatafetching/usedatafetching";
+import { FormEvent } from "react";
+
 export default function FormWithFetch(){
-    
+
+    const {value, handleChange} = useFormInput('');
+    const {values, runner} = useDataFetching();
+    const {loading, error, data} = values; 
+
+
     // The component should render a form with an input field and a button.
     return(
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={(eve) => onFormSubmit(eve)}>
             <label htmlFor="inp"> Input </label>
-            <input id="inp" type="text" onChange={(e: any) => onInputChange(e.target.value)}/>
+            {/* // Use the useFormInput custom hook to manage the state of the input field. */}
+            <input id="inp" type="text" onChange={(e: any) => handleChange(e.target.value)} value={value}/>
             <br/>
             <button type="submit"> FormWithFetch </button>
+            <p> {!loading && data} </p>
+            <p> {!loading && error} </p>
         </form>
     )
     
-    // Use the useFormInput custom hook to manage the state of the input field.
-    function onInputChange(e: string){
-
-    }
     
     // Use the useDataFetching custom hook to fetch data from the local dummy source when the form is submitted.
-    function onFormSubmit(){
-
+    function onFormSubmit(eve: FormEvent<HTMLFormElement>){
+        eve.preventDefault();
+        runner();
     }
 
     // Display the fetched data in the component when it becomes available.
